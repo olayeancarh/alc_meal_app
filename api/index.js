954c970aae78057f54940c37a34508d9d62b83c3
yinkaAkeju <akejuolayinka016@gmail.com>
2019-02-16 17:29:53 +0100

    Created the meal model

--- api/index.js
+++ api/index.js
@@ -4,10 +4,8 @@ const app = express();
 
 const PORT = 8001;
 
-app.get('/', (req, res) => {
-  return res.send('The API is working');
-});
+app.get('/', (req, res) => res.send('The API is working, yeah'));
 
 app.listen(PORT, () => {
-    console.log('Server is running');
+  console.log('Server is running');
 });
