ad73a58b717b7ef02684383317494561241d8e49
yinkaAkeju <akejuolayinka016@gmail.com>
2019-02-16 09:20:57 +0100

    fixed eslint error in indez file

--- api/index.js
+++ api/index.js
@@ -5,10 +5,9 @@ const app = express();
 const PORT = 8001;
 
 app.get('/', (req, res) => {
-    return res.send('The API is working');
+  return res.send('The API is working');
 });
 
 app.listen(PORT, () => {
     console.log('Server is running');
 });
-
