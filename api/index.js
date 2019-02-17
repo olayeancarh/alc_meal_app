2df6e5278f1cea808e8fb89edcf4513d92276bc0
olayeancarh <akejuolayinka016@gmail.com>
2019-02-17 16:57:55 +0100

    Meal controller was created and body-parser was installed

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const PORT = 8001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => res.send('The API is working, yeah'));
 
