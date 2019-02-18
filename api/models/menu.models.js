99d11a248a4bbc28588809032e837fe2a921f389
unknown <olatunde.haroun@serveconsulting.com>
2019-02-18 09:31:04 +0100

    Menu complete crud was done

---
+++ api/models/menu.models.js
@@ -0,0 +1,11 @@
+class Menu {
+    constructor(id, name, date_created, price) {
+      this.id = id;
+      this.name = name;
+      this.date_created = date_created;
+      this.price = price;
+    }
+  }
+  
+  export default Menu;
+  
--- api/utilz/dummyData.js
+++ api/utilz/dummyData.js
@@ -43,4 +43,48 @@ export default {
       price: 500,
     },
   ],
+  menus: [
+    {
+      id: 1,
+      name: 'Beans & Bread',
+      date_created: 'Mon Feb 18 2019 09:14:16 ',
+      price: 300,
+    },
+    {
+      id: 2,
+      name: 'Rice and Beans',
+      date_created: 'Mon Feb 18 2019 09:15:03',
+      price: 1200,
+    },
+    {
+      id: 3,
+      name: 'Mashed potatoes & egg',
+      date_created: 'Mon Feb 18 2019 09:15:18 ',
+      price: 700,
+    },
+    {
+      id: 4,
+      name: 'Jollof Rice & Beef',
+      date_created: 'Mon Feb 18 2019 09:15:34',
+      price: 700,
+    },
+    {
+      id: 5,
+      name: 'Eba & Vegetable',
+      date_created: 'Mon Feb 18 2019 09:15:52 ',
+      price: 700,
+    },
+    {
+      id: 6,
+      name: 'Fried Rice',
+      date_created: 'Mon Feb 18 2019 09:15:34',
+      price: 1500,
+    },
+    {
+      id: 7,
+      name: 'Yam Pottage',
+      date_created: 'Mon Feb 18 2019 09:16:14',
+      price: 500,
+    },
+  ],
 };
